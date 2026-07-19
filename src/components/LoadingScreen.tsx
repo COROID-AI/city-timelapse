959aa66cb510429b8c9f8384cd5a295cf280c007
Patrik Högberg <phogberg@users.noreply.github.com>
2026-07-20 01:23:35 +0200

    feat: City era timelapse 1945-2055 (Free)
    Create a 3D scene of a city block. Emphasis on detail is very important.
    
    The scene must have a timeline slider in the top, with the following options:
    1945, 1965, 1985, 2005, 2025 and 2055
    
    The point of the scene is to be able to select any of the 5 different years, and the scene will transform in front of your eyes to the time period selected from the slider.
    
    Time period should affect all aspects of the city block. The buildings, the vehicles, the storefronts, advertisements, outfits of the pedestrians, everything.
    
    This must be a polished high end scene with SFX, ability to navigate around and look at things, etc. Go all out.
    
    Generated by Coroid

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,13 +1,11 @@
-import { Html } from '@react-three/drei'
-
 export function LoadingScreen() {
   return (
-    <Html center>
+    <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50">
       <div className="flex flex-col items-center justify-center text-white">
         <div className="w-16 h-16 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin mb-4"></div>
         <p className="text-lg font-medium">Loading City Scene...</p>
         <p className="text-sm text-gray-400 mt-2">Preparing time period assets</p>
       </div>
-    </Html>
+    </div>
   )
 }
